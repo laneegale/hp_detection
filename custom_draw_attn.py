c165994d53079359c4d81211ee37eb3744bb943b
tom <tom-kccheng@gmail.com>
2026-01-17 22:31:22 -0500

    fixed a bug

--- custom_draw_attn.py
+++ custom_draw_attn.py
@@ -17,10 +17,10 @@ from tqdm import tqdm
 from helpers import get_model_and_transform
 from draw_attn import draw_attention_mask
 
-data_dir = "/Z/cuhk_data/HPACG/final"
+data_dir = "/mnt/x/Tom_Cheng/HPACG_data/final"
 data_dir = Path(data_dir)
 model_name = "conch_v15"
-out_fp = "./results/attn"
+out_fp = "/mnt/x/Tom_Cheng/HPACG_data/attn"
 out_fp = Path(out_fp)
 import os
 
@@ -48,7 +48,7 @@ t_ls_m = {
 }
 
 for t in t_ls:
-    os.makedirs(out_fp/t)
+    os.makedirs(out_fp/model_name/t, exist_ok=True)
 
 if __name__ == "__main__":
     model, transform_func = get_model_and_transform(model_name)
@@ -56,11 +56,11 @@ if __name__ == "__main__":
     with open(f"results/{model_name}.pkl", "rb") as f:
         results = pickle.load(f)
 
-    for t in t_ls:
+    for t in t_ls[:]:
         print(f"Processing {t}")
         for i in tqdm(results[f"fp_{t}"], unit="imgs"):
             img_path = data_dir / t_ls_m[t] / os.path.basename(i)
-            out_path = out_fp / t / os.path.basename(i)
+            out_path = out_fp / model_name / t / os.path.basename(i)
             draw_attention_mask(model, transform_func, img_path, model_name, title=None, save_fp=out_path)
 
 
